0fd36c81fbc93d0ba68359982c115a55b1e44a42
MOHIT <moit16ee@cmrit.ac.in>
2019-10-08 07:59:07 +0530

    Updated

--- nmap2.py
+++ nmap2.py
@@ -3,4 +3,8 @@ import subprocess
 
 host = input("Enter The Host Add:-")
 print("."*50)
-subprocess.check_call('')
+subprocess.check_call(['nmap','-n','-V','-Pn','-sn','-sL','PE','-PP','-oN', 'host.txt', host])
+
+port_range = input("Enter The Host Add:-")
+print("."*50)
+subprocess.check_call(['nmap','-p','-l-500','-oN', 'port_range.txt', port_range])
